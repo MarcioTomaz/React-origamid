273128bcc1e7c443bb2767257fd41eac83e79f77
MarcioTomaz <marciotmaz@gmail.com>
2022-08-25 11:19:08 -0300

    jsx 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 const App = () => {
+
+  const nome = 'Marzio';
+  const number = Math.random();
+  const ativo = false;
+
+  function mostrarNome(sobrenome){
+      return 'Marzio ' + sobrenome; 
+  }
+
+  const carro = {
+    marca: 'Ford', 
+    portas: '4'
+  }
+
+  const estiloP = {
+    color: "blue"
+  }
+
   return (
-    <div >
-      Meu app
-    </div>
+    <>
+      <a 
+        href="https://pokeapi.co/" 
+        title="pokemon api"
+        className="ativo">Meu app
+      </a>
+
+      <label htmlFor="nome">
+        <input type="text" id="nome" />
+      </label>
+
+      <p style={estiloP}>{mostrarNome('Tomaz')}</p>
+
+      <p>{carro.marca}</p>
+      
+      <p>{number * 1000 / 5 - 10 }</p>
+
+      <p className={ ativo? 'ativo': 'inativo'}>condicional</p>
+    </>
   );
 }
 
