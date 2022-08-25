2934ea34dd9f253ede5d04cae18c5d9e5d6dba8d
MarcioTomaz <marciotmaz@gmail.com>
2022-08-25 13:40:58 -0300

    arrays map e filtro

--- src/App.js
+++ src/App.js
@@ -1,50 +1,45 @@
 
 
-
 const App = () => {
-  // Mostre os dados da aplicação, como aprensetado no vídeo
-  // Não utilize CSS externo, use o style para mudar as cores
-  // Se a situação estiver ativa pinte de verde, inativa vermelho
-  // Se o gasto for maior que 10000 mostre uma mensagem
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
+  const produtos = ['Notebook, ', 'Tablet, ', 'Smartphone'];
+  const produtos2 = [<li key="e1">Notebook</li>, <li key="e2">Celular</li>];
 
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
 
-    const dados = mario;
 
-    // pegando primeiro os preços do objeto e retirando o 'R$ ' e convertendo para numero com o 
-    // Number()
-    const total = dados.compras.map( item => Number(item.preco.replace('R$ ', '')))
-    .reduce((a,b) => a + b);
-    //reduce ele recebe o valor anterior e o atual, e dps a expressao 
+  // return <div>{produtos2}</div>;
+  // return <ul>{produtos.map(produto => <li key={produto}>produto</li>)}</ul>
 
-    return <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>Situação: <span style={{color: dados.ativa? 'green':'red'}}>{dados.ativa ? 'Ativa' : 'Inativa'}</span></p>
-      <p>Total gasto: R$ {total}</p>
-      <p>{total > 10000 ? "Você está fora do limite" : ''}</p>
+  // return (
+  //   <ul>
+  //     {livros.map((livro) =>
+  //       <li key={livro.nome}>{livro.nome}</li>
+  //     )}
+  //   </ul>
+  // )
 
-    </div>;  
+  // return (
+  //   <ul>
+  //     {livros.map(({nome, ano}) => // desestruturando o objeto
+  //       <li key={nome}>{nome}, {ano}</li>
+  //     )}
+  //   </ul>
+  // )
+
+  return (
+    <ul>
+      {livros
+        .filter(({ano}) => ano >= 1998)// filtrando pelo a partir do ano 1998
+        .map(({nome, ano}) => // filtrando pelo ano
+        <li key={nome}>{nome}, {ano}</li>
+      )}
+    </ul>
+  )
+  
 }
 
 export default App;
