9f82c56dae269904b15a0ca2a49e4d929acc2b89
MarcioTomaz <marciotmaz@gmail.com>
2022-08-25 14:02:02 -0300

    add exercicio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 
 const App = () => {
-  const produtos = ['Notebook, ', 'Tablet, ', 'Smartphone'];
-  const produtos2 = [<li key="e1">Notebook</li>, <li key="e2">Celular</li>];
+  // const produtos = ['Notebook, ', 'Tablet, ', 'Smartphone'];
+  // const produtos2 = [<li key="e1">Notebook</li>, <li key="e2">Celular</li>];
 
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
+  // const livros = [
+  //   { nome: 'A Game of Thrones', ano: 1996 },
+  //   { nome: 'A Clash of Kings', ano: 1998 },
+  //   { nome: 'A Storm of Swords', ano: 2000 },
+  // ];
 
 
   // return <div>{produtos2}</div>;
@@ -30,16 +30,55 @@ const App = () => {
   //   </ul>
   // )
 
-  return (
-    <ul>
-      {livros
-        .filter(({ano}) => ano >= 1998)// filtrando pelo a partir do ano 1998
-        .map(({nome, ano}) => // filtrando pelo ano
-        <li key={nome}>{nome}, {ano}</li>
-      )}
-    </ul>
-  )
+  // return (
+  //   <ul>
+  //     {livros
+  //       .filter(({ano}) => ano >= 1998)// filtrando pelo a partir do ano 1998
+  //       .map(({nome, ano}) => // filtrando pelo ano
+  //       <li key={nome}>{nome}, {ano}</li>
+  //     )}
+  //   </ul>
+  // )
   
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+  const produtos = [
+    {
+      id: 1,
+      nome: 'Smartphone',
+      preco: 'R$ 2000',
+      cores: ['#29d8d5', '#252a34', '#fc3766'],
+    },
+    {
+      id: 2,
+      nome: 'Notebook',
+      preco: 'R$ 3000',
+      cores: ['#ffd045', '#d4394b', '#f37c59'],
+    },
+    {
+      id: 3,
+      nome: 'Tablet',
+      preco: 'R$ 1500',
+      cores: ['#365069', '#47c1c8', '#f95786'],
+    },
+  ];
+
+  const dadosFiltrados = produtos.filter(({preco}) => Number(preco.replace('R$ ', '')) > 1500);
+  console.log('dados: ', dadosFiltrados)
+  
+  return (
+    <>
+    <section>
+      {dadosFiltrados.map(({id, nome, preco, cores}) => <div key={id}>
+        <h1>{nome}</h1>
+        <p>{preco}</p>
+        <ul>
+          {cores.map( cor => <li style={{backgroundColor: cor, color: 'white'}} key={cor}>{cor}</li>)}
+        </ul>
+      </div>)}
+    </section>
+    </>
+  )
 }
 
 export default App;
