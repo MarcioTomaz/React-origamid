e319fc7e7103ce08fadbffe749376fa74fdce56a
MarcioTomaz <marciotmaz@gmail.com>
2022-08-26 16:21:29 -0300

    add use ref ao input

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
 
 import React from "react";
-import Produto from "./Produto";
-// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-// Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
 const App = () => {
+  const video = React.useRef();
 
-  const [produto, setProduto] = React.useState(null);
-
+  const [comentarios, setComentarios] = React.useState([]);
+  const [input,setInput] = React.useState('');
+  
   React.useEffect( () => {
-    const produtoLocal = window.localStorage.getItem('produto');
+    console.log(video.current.currentTime);
+  });
 
-    if(produtoLocal !== null){
-      setProduto(produtoLocal);
-    }
-    console.log(produtoLocal)
-  }, [])  
+  const inputElement = React.useRef();//transforma em uma referencia
 
-  React.useEffect( () => {
-    if(produto !== null){
-      window.localStorage.setItem('produto', produto);
-    }
-  }, [ produto ])
-
-  function handleClick({target}){
-      setProduto(target.innerText);
+  function handleClick(){
+    setComentarios([...comentarios, input])//usando o spread ...
+    setInput('');
+    inputElement.current.focus();//pega a referencia e seta o metodo focus
   }
 
-  return (  
-    <>    
-    <h1>Preferência: {produto}</h1>
+  return(
+    <>
+      <video ref={video} />    
 
-    <button onClick={handleClick} style={{marginRight: '1rem'}}>notebook</button>
-    <button onClick={handleClick} >smartphone</button>
-    <Produto produto={produto}/>
+      <div>
+        <ul>{comentarios.map( (comentario, index) => <li key={index}> {comentario}</li>)}      
+        </ul>
+        <input 
+          type="text" 
+          ref={inputElement}//referencia ao input
+          value={input} 
+          onChange={ ({target}) => setInput(target.value)} 
+          />
+        <br />
+        
+        <button onClick={handleClick}>Enviar</button>
+      </div>
     </>
-  )
+  ) 
 }
 
 export default App;
