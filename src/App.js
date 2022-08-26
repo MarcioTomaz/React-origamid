86396aebfeb5e81ee37250112405cacf53b71d44
MarcioTomaz <marciotmaz@gmail.com>
2022-08-26 15:59:55 -0300

    add exercicio

--- src/App.js
+++ src/App.js
@@ -1,50 +1,43 @@
 
 import React from "react";
 import Produto from "./Produto";
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
 const App = () => {
 
-  const [ contar, setContar] = React.useState(0);
-  const [dados, setDados] = React.useState(null);
-  const [ativo, setAtivo] = React.useState(false);
-  
-  React.useEffect(() => {
-    // se o fetch estivesse fora do useEffect, toda vez que o componente
-    // fosse atualizado, o mesmo seria executado
-    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
-      .then((response) => response.json())
-      .then((json) => setDados(json));
-  }, []);
+  const [produto, setProduto] = React.useState(null);
 
+  React.useEffect( () => {
+    const produtoLocal = window.localStorage.getItem('produto');
 
-  console.log('Executou fora')
- // Uma Array vazia indica que o efeito não possui nenhum dependência,
-  // assim o mesmo só irá ocorrer quando o componente é renderizado inicialmente (montado)
-  // O efeito ocorre logo após a renderização do mesmo
-  React.useEffect(() => {
-    console.log('Apenas quando renderiza');
-  }, []);
+    if(produtoLocal !== null){
+      setProduto(produtoLocal);
+    }
+    console.log(produtoLocal)
+  }, [])  
 
-  React.useEffect(() => {
-    document.title = 'Total ' + contar;
-  }, [contar]);// no [] fica a dependencia 
+  React.useEffect( () => {
+    if(produto !== null){
+      window.localStorage.setItem('produto', produto);
+    }
+  }, [ produto ])
 
+  function handleClick({target}){
+      setProduto(target.innerText);
+  }
 
   return (  
-    <>  
-      {dados && <div>
-          <h1>{dados.nome}</h1>
-          <p>R$: {dados.preco * contar}</p>
-        
-        </div>}
-      <button onClick={ () => setContar(contar + 1)}>{contar}</button>
+    <>    
+    <h1>Preferência: {produto}</h1>
 
-      <br/>
-      <br/>
-
-      <button onClick={ () => setAtivo(!ativo)}>Ativar</button>
-      {ativo && <Produto />}
-
+    <button onClick={handleClick} style={{marginRight: '1rem'}}>notebook</button>
+    <button onClick={handleClick} >smartphone</button>
+    <Produto produto={produto}/>
     </>
   )
 }
--- src/Produto.js
+++ src/Produto.js
@@ -1,25 +1,24 @@
 
 import React from 'react'
 
-const Produto = () => {
+const Produto = ({produto}) => {
+
+  const [dados, setDados] = React.useState(null);
 
   React.useEffect( () => {
-    function handleScroll(event){
-      console.log(event)
+    if(produto !== null){
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+      .then( response => response.json()).
+      then((json) => setDados(json));
     }
+  }, [produto])// sempre q o produto alterar é para fazer a requisicao
+
+  if(dados === null )return null;
+  return <div>
+    <h1>{dados.nome}</h1>
+    <h1>{dados.preco}</h1>
+  </div>;
   
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, [])
-
-  return (
-    <div style={{height: '200vh' }}>
-      <p>Meu produto</p>
-    </div>
-  )
 }
 
 export default Produto
