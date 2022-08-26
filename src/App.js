9f2728dbca6549a8ec1c5129ce7f6101c1a38280
MarcioTomaz <marciotmaz@gmail.com>
2022-08-26 16:32:38 -0300

    use ref timeout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import React from "react";
 
 const App = () => {
 
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notificacao, setNotificacao] = React.useState(null);
+
+  const timeoutRef = React.useRef();
+  
+
+  function handleClick(){
+    setCarrinho(carrinho + 1);
+    setNotificacao('Item adicionado ao carrinho');
+
+    clearTimeout(timeoutRef.current)
+    timeoutRef.current = setTimeout( () => {
+      setNotificacao(null)
+    }, 2000)
+    console.log(timeoutRef.current)
+  }
+
   return(
     <>
+      <div>
+        <p>{notificacao}</p>
+        <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
+      </div>
     </>
   ) 
 }
