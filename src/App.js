58072d4daa33f8fb18449a9ec5f85e4826f76bbc
MarcioTomaz <marciotmaz@gmail.com>
2022-08-26 14:56:14 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,44 +3,50 @@ import React from "react";
 import Produto from "./Produto";
 
 const App = () => {
-  // Os links abaixo puxam dados de um produto em formato JSON
-  // https://ranekapi.origamid.dev/json/api/produto/tablet
-  // https://ranekapi.origamid.dev/json/api/produto/smartphone
-  // https://ranekapi.origamid.dev/json/api/produto/notebook
-  // Crie uma interface com 3 botões, um para cada produto.
-  // Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-  // Mostre apenas um produto por vez
-  // Mostre a mensagem carregando... enquanto o fetch é realizado
 
-  const [ dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [ contar, setContar] = React.useState(0);
+  const [dados, setDados] = React.useState(null);
+  const [ativo, setAtivo] = React.useState(false);
+  
+  React.useEffect(() => {
+    // se o fetch estivesse fora do useEffect, toda vez que o componente
+    // fosse atualizado, o mesmo seria executado
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((json) => setDados(json));
+  }, []);
 
-  async function handleClick(event){
-    setCarregando(true);
 
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`);
+  console.log('Executou fora')
+ // Uma Array vazia indica que o efeito não possui nenhum dependência,
+  // assim o mesmo só irá ocorrer quando o componente é renderizado inicialmente (montado)
+  // O efeito ocorre logo após a renderização do mesmo
+  React.useEffect(() => {
+    console.log('Apenas quando renderiza');
+  }, []);
 
-    const json = await response.json();
-    console.log(json);
-    setDados(json);
-    setCarregando(false);
-  }
+  React.useEffect(() => {
+    document.title = 'Total ' + contar;
+  }, [contar]);// no [] fica a dependencia 
+
 
   return (  
-    <>
-      <button onClick={handleClick}>tablet</button>
-      <button onClick={handleClick}>Smartphone</button>
-      <button onClick={handleClick}>Notebook</button>
+    <>  
+      {dados && <div>
+          <h1>{dados.nome}</h1>
+          <p>R$: {dados.preco * contar}</p>
+        
+        </div>}
+      <button onClick={ () => setContar(contar + 1)}>{contar}</button>
 
-       {/*{dados ? <Produto dados={dados} /> : 'Sem produtos'}; versao com ternario */}
+      <br/>
+      <br/>
 
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados &&<Produto dados={dados}/>}
-      {/* com o && só irá executar se o dados for vdd, se n for nulo */}
+      <button onClick={ () => setAtivo(!ativo)}>Ativar</button>
+      {ativo && <Produto />}
 
-      
     </>
   )
-};
+}
 
 export default App;
--- src/Produto.js
+++ src/Produto.js
@@ -1,13 +1,23 @@
 
 import React from 'react'
 
-const Produto = ({dados}) => {
-  return (
-    <div>
-        <h1>{dados.nome}</h1>
-        <p>R$: {dados.preco}</p>
-        <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+const Produto = () => {
 
+  React.useEffect( () => {
+    function handleScroll(event){
+      console.log(event)
+    }
+  
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  return (
+    <div style={{height: '200vh' }}>
+      <p>Meu produto</p>
     </div>
   )
 }
