449f36e01c78c7f19fc09ac2c56d8a93ae4fbd12
MarcioTomaz <marciotmaz@gmail.com>
2022-08-26 14:15:48 -0300

    exercicio usestate

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 
 import React from "react";
-import ButtonModal from "./ButtonModal";
-import Modal from "./Modal";
+import Produto from "./Produto";
 
 const App = () => {
+  // Os links abaixo puxam dados de um produto em formato JSON
+  // https://ranekapi.origamid.dev/json/api/produto/tablet
+  // https://ranekapi.origamid.dev/json/api/produto/smartphone
+  // https://ranekapi.origamid.dev/json/api/produto/notebook
+  // Crie uma interface com 3 botões, um para cada produto.
+  // Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+  // Mostre apenas um produto por vez
+  // Mostre a mensagem carregando... enquanto o fetch é realizado
 
-  const [ contar, setContar]= React.useState(1);
-  const [items1, setItems1] = React.useState('Item 1');
+  const [ dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
-  // const [modal, setModal] = React.useState(() => {//callback pegando do localStorage
-  //   const ativo = window.localStorage.getItem('ativo');
-  //   return ativo;
-  // });
+  async function handleClick(event){
+    setCarregando(true);
 
-  const [modal, setModal] = React.useState(true);
+    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`);
 
-  const [items, setItems] = React.useState('Teste');
-  
-  function handleClick(){
-    setItems('outros');
+    const json = await response.json();
+    console.log(json);
+    setDados(json);
+    setCarregando(false);
   }
 
-  function contadorClick(){
-    setContar((contar) => {
-      return contar +1;
-    })
-    
-    setItems((items) => [...items, 'Item ' + (contar + 1)])
-  }
-  
   return (  
     <>
-      <div>   
-        <p>{items}</p>
-        <button onClick={handleClick}>CLicar</button>
-      </div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>Smartphone</button>
+      <button onClick={handleClick}>Notebook</button>
 
-      {items1.localeCompare( item => <li key={items}>{item}</li>)}
-      <button onClick={contadorClick}>{contar}</button>
+       {/*{dados ? <Produto dados={dados} /> : 'Sem produtos'}; versao com ternario */}
 
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados &&<Produto dados={dados}/>}
+      {/* com o && só irá executar se o dados for vdd, se n for nulo */}
+
+      
     </>
   )
 };
