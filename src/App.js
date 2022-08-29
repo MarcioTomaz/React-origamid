7bfc642f69b049078a3d3065911bc18e0b4f3f0b
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 02:04:45 -0300

    useFetch2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
 import React from "react";
+import useFetch from "./useFetch";
 
 const App = () => {
 
-  return(
-    <>
-    </>
-  ) 
+  const {request, data, loading, error} = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData(){
+    const {response, json} = await request('https://ranekapi.origamid.dev/json/api/produto/');   
+    console.log(response)
+
+    }
+    fetchData();
+  }, [request]);
+
+  if(error) return <p>{error}</p>
+  if(loading) return <p>Carregando...</p>
+  if(data)
+    return(
+      <>
+      {data.map( (produto) => <div key={produto.id}>{produto.nome}</div>)}
+      </>
+    ) 
+    else return null
 }
 
 export default App;
