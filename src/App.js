3fa13d6a20e4a1788c188cc1d2da1ebb70ae18c2
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 13:50:45 -0300

    add input 1

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import React from "react";
 
 const App = () => {
 
-  const [ nome, setNome] = React.useState('');
-  const [ email, setEmail] = React.useState('');
+  // const [ nome, setNome] = React.useState('');
+  // const [ email, setEmail] = React.useState('');  
 
   function handleSubmit(event){
     event.preventDefault();
     console.log(event);
   }
 
+  const [ form , setForm ] = React.useState({
+    nome:'',
+    email:'',
+  })
+
+  function handleChance({target}){
+    const {id, value} = target;
+    console.log(id,value);
+    setForm({ ...form, [id]: value })//passando dentro do [] para ele reconhecer como se fosse uma propriedade no caso "nome" ou "email". o ...form é para passar tmb os valores anteriores
+  }
+
   return(
     <>
 
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <label htmlFor="nome">Nome</label>
         <input 
           id="nome" 
@@ -33,7 +44,31 @@ const App = () => {
           onChange={(event) => setEmail(event.target.value)}/> 
         
         <button>Enviar</button>    
+      </form> */}
+
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="nome">Nome</label>
+          <input 
+            id="nome" 
+            type="text"
+            name="nome" 
+            value={form.nome} 
+            onChange={handleChance}/>   
+
+        <label htmlFor="email">Email</label>
+
+          <input 
+            id="email" 
+            type="email"
+            name="email" 
+            value={form.email} 
+            onChange={handleChance}/> 
+          
+          <button>Enviar</button>    
       </form>
+
+      {form.email}
+      {form.nome}
     </>
   ) 
 }
