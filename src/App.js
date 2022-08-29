1886f5a946a82c4fcd781305d6ff196661a1f823
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 00:39:27 -0300

    exemplo simples do userContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
+import Produto from "./Produto";
+import UserContext from "./UserContext";
 
 const App = () => {
-
+  console.log(UserContext);
   return(
-    <>
-    </>
+    <UserContext.Provider value={{nome:'Marcio'}}>
+      {/* Agora o <Produto /> consegue ter acesso ao contexto do UserContext */}
+      <Produto />      
+    </UserContext.Provider>
   ) 
 }
 
