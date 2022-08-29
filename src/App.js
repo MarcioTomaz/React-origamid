6b7468950d7aab78c3568934b4b5a7fd38e68f9a
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 01:38:34 -0300

    useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import React from "react";
+import useLocalStorage from './useLocalStorage'
 
 const App = () => {
+  const [ produto, setProduto] = useLocalStorage('produto', '');
+
+  function handleClick({target}){
+      setProduto(target.innerText)
+  }
 
   return(
     <>
+    <button onClick={handleClick}>notebook</button>
+    <button onClick={handleClick}>pc</button>
     </>
   ) 
 }
