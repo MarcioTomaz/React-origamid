cfc4a4000fb42bd3e8f333b0558e5832376daa41
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 01:24:28 -0300

    exercicio useContext

---
+++ src/Produto.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { GlobalContext } from './GlobalContext'
+
+function Produto() {
+  const global = React.useContext(GlobalContext);
+  console.log(global);
+
+  if(global.dados === null) return null
+
+  return <div>Produto: {global.dados.map( produto => <li key={produto.id}>{produto.nome}</li>)}</div>
+    
+  
+}
+
+export default Produto
