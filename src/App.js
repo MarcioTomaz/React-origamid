8a7a2f4be96fda60392958c51754fecddc3469bd
MarcioTomaz <marciotmaz@gmail.com>
2022-08-29 15:07:33 -0300

    select

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React from "react";
 
 const App = () => {
 
+  const [select, setSelect] = React.useState('');
+
   return(
-    <>
-    </>
+    <form>
+      <select value={select} onChange={({target}) => setSelect(target.value) } id="produtos">
+        <option disabled value="">Selecione</option>
+        <option value="notebook">Notebook</option>
+        <option value="pc">pc</option>
+        <option value="celular">celular</option>
+      </select>
+      {select}
+    </form>
   ) 
 }
 
