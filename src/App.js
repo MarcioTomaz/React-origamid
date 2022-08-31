be0ec33d1e0048341b9be5d79788715ab516f30b
MarcioTomaz <marciotmaz@gmail.com>
2022-08-31 16:56:47 -0300

    add react bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React from "react";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
+import Card from 'react-bootstrap/Card';
+import CardHeader from "react-bootstrap/esm/CardHeader";
+
+const App = () => { 
 
   return(
     <>
+
+    <h1 className="mx-5 my-5">Bootstrap padrao</h1>
       <div className="card bg-dark text-white m-5">
         <div className="card-header">Notebook</div>
         <div className="card-body">
@@ -14,6 +19,19 @@ const App = () => {
           <p className="card-text">Esse notebook é toper dmais</p>
         </div>
       </div>
+
+      <hr />
+
+      <Card bg="dark" text="white" style={{ maxWidth: '18rem' }} className="m-5">
+        <Card.Header>Notebook</Card.Header>
+        <Card.Body>
+          <Card.Title>R$ 2500</Card.Title>
+          <Card.Text>
+            Esse é um notebook com 1tb, 16gb de ram e placa de vídeo de 16gb.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
     </>
   ) 
 }
