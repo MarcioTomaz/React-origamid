61404ade86b114b903f9ff69a1225b532a0ffed6
MarcioTomaz <marciotmaz@gmail.com>
2022-08-31 18:06:31 -0300

    images 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 
 import React from "react";
+import foto from './img/foto.jpg';
+import './App.css';
+// import dog from './img/dog.svg';
+import { ReactComponent as Dog} from './img/dog.svg';
+import DogSvg from "./DogSvg";
+
 
 const App = () => {
+  const [olho, setOlho] = React.useState(0);
 
-  return(
-    <>
-    </>
-  ) 
-}
+  function handleClick(){
+    for(let i = 0; i < 5; i++){
+      setTimeout( () => {
+          setOlho(i);
+      }, 30 * i)
+    }
+  }
+
+
+  return (
+    <div>
+      {/* <Dog /> */}
+      <p className="fundo" onClick={handleClick} />
+      <Dog/>
+      <DogSvg color="green" olho={olho}/>   
+      <DogSvg color="red" olho={olho}/>   
+      <DogSvg color="yellow" olho={olho}/>   
+
+      {/* <img src={dog} alt="dog" /> */}
+      <img src={foto} alt="cachorro" />   
+
+    </div>
+  );
+};
+
 
 export default App;
