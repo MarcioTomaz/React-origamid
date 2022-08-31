fa1e34cdd7124c0d93b07e4e9a008825c8b6d67a
MarcioTomaz <marciotmaz@gmail.com>
2022-08-31 16:43:43 -0300

    add bootstrap padrao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import React from "react";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
   return(
     <>
+      <div className="card bg-dark text-white m-5">
+        <div className="card-header">Notebook</div>
+        <div className="card-body">
+          <h5 className="card-title">R$ 2500</h5>
+          <p className="card-text">Esse notebook é toper dmais</p>
+        </div>
+      </div>
     </>
   ) 
 }
