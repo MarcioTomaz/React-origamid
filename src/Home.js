96652f97913e5f2d8fa9571f48c723303cd0eea5
MarcioTomaz <marciotmaz@gmail.com>
2022-09-01 14:40:14 -0300

    helmet

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
 
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import Head from './Head'
+import {Helmet} from 'react-helmet';
 
 const Home = () => {
   return (
     <>
+      {/* <Head title="Home" description="Essa é a descrição da home"/> */}
+      <Helmet >
+        <title>Dogs | Home</title>
+        <meta name="description" content="Essa é a página home"></meta>
+      </Helmet>
       <h1>Home</h1>
       <p>Essa é a home</p>
       <Link to="produto/notebook">Notebook</Link>{''} |
--- src/Sobre.js
+++ src/Sobre.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Head from './Head'
 
 const Sobre = () => {
   return (
     <>
+      <Head title="Sobre" description="Essa é a descrição da Sobre"/>
+
       <h1>Pag sobre</h1>
       <p>É a pag sobre</p>
     </>
